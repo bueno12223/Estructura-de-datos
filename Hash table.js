58dc2431d0b0c80efbf4c37062fbbbf75400c474
Jesus Berrio <berriojesus122@gmail.com>
2020-10-20 12:05:55 -0400

    primera hash table creada

---
+++ Hash table.js
@@ -0,0 +1,18 @@
+class myArray {
+    constructor(){
+        this.length = 0
+        this.data = {}
+    }
+    get(index){
+        return this.data[index]
+    }
+    push(item){
+        this.data[this.length] = item
+        this.length++
+        return this.data
+    }
+    pop(){
+        this.length--
+    }
+
+}
